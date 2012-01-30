b473334b0d7b1ac9538ee0e38881e5fcdf4edcca
Archit Taneja <archit@ti.com>
2012-01-30 13:58:28 +0530

    OMAPDSS: HDMI: Remove core_av_offset usage in OMAP5 HDMI core
    The struct hdmi_ip_data provides 2 offsets for the HDMI core register
    addresses. This made sense for OMAP4 HDMI core which had 2 different
    base addresses for sys and av registers respectively, which is not the
    case in OMAP5. Add a dss features function which returns the hdmi core's
    offset for different OMAPs.
    
    Signed-off-by: Archit Taneja <archit@ti.com>
    Signed-off-by: Mythri P K <mythripk@ti.com>

--- drivers/video/omap2/dss/dss_features.c
+++ drivers/video/omap2/dss/dss_features.c
@@ -58,6 +58,8 @@ struct omap_dss_features {
 	const u32 burst_size_unit;
 
 	const int dsi_ddr_div;
+
+	const u32 hdmi_core_sys_offset;
 };
 
 /* This struct is assigned to one of the below during initialization */
@@ -599,6 +601,7 @@ static const struct omap_dss_features omap4430_es1_0_dss_features  = {
 	.buffer_size_unit = 16,
 	.burst_size_unit = 16,
 	.dsi_ddr_div = 4,
+	.hdmi_core_sys_offset = 0x400,
 };
 
 /* For OMAP4430 ES 2.0, 2.1 and 2.2 revisions */
@@ -620,6 +623,7 @@ static const struct omap_dss_features omap4430_es2_0_1_2_dss_features = {
 	.buffer_size_unit = 16,
 	.burst_size_unit = 16,
 	.dsi_ddr_div = 4,
+	.hdmi_core_sys_offset = 0x400,
 };
 
 /* For all the other OMAP4 versions */
@@ -641,6 +645,7 @@ static const struct omap_dss_features omap4_dss_features = {
 	.buffer_size_unit = 16,
 	.burst_size_unit = 16,
 	.dsi_ddr_div = 4,
+	.hdmi_core_sys_offset = 0x400,
 };
 
 /* OMAP5 DSS Features */
@@ -662,6 +667,7 @@ static const struct omap_dss_features omap5_dss_features = {
 	.buffer_size_unit = 16,
 	.burst_size_unit = 16,
 	.dsi_ddr_div = 2,
+	.hdmi_core_sys_offset = 0x20000,
 };
 
 #if defined(CONFIG_OMAP4_DSS_HDMI)
@@ -759,6 +765,11 @@ u32 dss_feat_get_burst_size_unit(void)
 int dss_feat_get_dsi_ddr_div(void)
 {
 	return omap_current_dss_features->dsi_ddr_div;
+}
+
+unsigned long dss_feat_get_hdmi_core_sys_offset(void)
+{
+	return omap_current_dss_features->hdmi_core_sys_offset;
 }
 
 /* DSS has_feature check */
