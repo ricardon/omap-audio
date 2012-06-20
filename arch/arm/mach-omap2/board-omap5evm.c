856fefeb07bd7a175b271c41ff481b5f6648301a
Archit Taneja <archit@ti.com>
2012-06-20 14:25:11 +0530

    OMAP5: EVM: Make HDMI HPD GPIO independent of PALMAS IC
    Palmas IC is not related to HDMI output nor its HPD input. Hence,
    remove dependency.
    
    Signed-off-by: Ricardo Neri <ricardo.neri@ti.com>

--- arch/arm/mach-omap2/board-omap5evm.c
+++ arch/arm/mach-omap2/board-omap5evm.c
@@ -35,10 +35,10 @@
 #include <video/omapdss.h>
 #include <video/omap-panel-lg4591.h>
 
+#define HDMI_GPIO_HPD 193
+
 #ifdef CONFIG_OMAP5_SEVM_PALMAS
 #define OMAP5_GPIO_END	0
-
-#define HDMI_GPIO_HPD 193
 
 static struct palmas_gpadc_platform_data omap5_palmas_gpadc = {
 	.ch3_current = 0,
