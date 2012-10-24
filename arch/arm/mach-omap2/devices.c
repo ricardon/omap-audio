838348e5188a9548c5e5ed94aca16d92030c949e
Ricardo Neri <ricardo.neri@ti.com>
2012-10-23 19:15:54 -0500

    ARM: OMAP4+: Relocate the creation of HDMI device to board files
    The purpose of the device being relocated is to describe how the
    HDMI output is wired on the 4430SDP and OMAP4 Panda boards. Hence,
    it makes sense to relocate them on the applicable board files.
    
    This device is used by the ASoC OMAP HDMI machine driver.
    
    Signed-off-by: Ricardo Neri <ricardo.neri@ti.com>

--- arch/arm/mach-omap2/devices.c
+++ arch/arm/mach-omap2/devices.c
@@ -382,22 +382,6 @@ static void __init omap_init_dmic(void)
 static inline void omap_init_dmic(void) {}
 #endif
 
-#if defined(CONFIG_SND_OMAP_SOC_OMAP_HDMI) || \
-		defined(CONFIG_SND_OMAP_SOC_OMAP_HDMI_MODULE)
-
-static struct platform_device omap_hdmi_audio = {
-	.name	= "omap-hdmi-audio",
-	.id	= -1,
-};
-
-static void __init omap_init_hdmi_audio(void)
-{
-	platform_device_register(&omap_hdmi_audio);
-}
-#else
-static inline void omap_init_hdmi_audio(void) {}
-#endif
-
 #if defined(CONFIG_SND_OMAP_SOC_ABE) || \
 	defined(CONFIG_SND_OMAP_SOC_ABE_MODULE)
 
@@ -665,7 +649,6 @@ static int __init omap2_init_devices(void)
 	 */
 	omap_init_audio();
 	omap_init_camera();
-	omap_init_hdmi_audio();
 	omap_init_mcasp();
 	omap_init_mbox();
 	/* If dtb is there, the devices will be created dynamically */
