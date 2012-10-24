ed1ed46060bb329bf0d0dbcae3738bf37a7fa917
Ricardo Neri <ricardo.neri@ti.com>
2012-10-23 19:07:29 -0500

    OMAPDSS: HDMI: Init HDMI operations in _hw_probe
    Initialize the HDMI operations from omapdss_hdmihw_probe as there is no need
    to wait until hdmi_init_display.
    
    TODO: Need to handle errors in hdmi_probe_pdata.
    
    Signed-off-by: Ricardo Neri <ricardo.neri@ti.com>

--- drivers/video/omap2/dss/hdmi.c
+++ drivers/video/omap2/dss/hdmi.c
@@ -336,8 +336,6 @@ static int __init hdmi_init_display(struct omap_dss_device *dssdev)
 
 	DSSDBG("init_display\n");
 
-	dss_init_hdmi_ip_ops(&hdmi.ip_data, omapdss_get_version());
-
 	if (hdmi.vdda_hdmi_dac_reg == NULL) {
 		struct regulator *reg;
 
@@ -1086,6 +1084,8 @@ static int __init omapdss_hdmihw_probe(struct platform_device *pdev)
 
 	mutex_init(&hdmi.lock);
 	mutex_init(&hdmi.ip_data.lock);
+
+	dss_init_hdmi_ip_ops(&hdmi.ip_data, omapdss_get_version());
 
 	res = platform_get_resource(hdmi.pdev, IORESOURCE_MEM, 0);
 	if (!res) {
