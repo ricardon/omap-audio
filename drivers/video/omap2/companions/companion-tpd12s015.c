c2aaeada3abe403e8a883195b03a27bbb1de55c7
Ricardo Neri <ricardo.neri@ti.com>
2012-11-14 19:47:47 -0600

    OMAPDSS: TPD12S015: More updates - squash
    Signed-off-by: Ricardo Neri <ricardo.neri@ti.com>

--- drivers/video/omap2/companions/companion-tpd12s015.c
+++ drivers/video/omap2/companions/companion-tpd12s015.c
@@ -2,7 +2,7 @@
  * tpd12s015.c
  *
  * HDMI interface DSS driver setting for TI's OMAP4 family of processor.
- * Copyright (C) 2010-2011 Texas Instruments Incorporated - http://www.ti.com/
+ * Copyright (C) 2012 Texas Instruments Incorporated - http://www.ti.com/
  * Author: Ricardo Neri <ricardo.neri@ti.com>
  *
  * This program is free software; you can redistribute it and/or modify it
@@ -17,53 +17,86 @@
  * You should have received a copy of the GNU General Public License along with
  * this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
+#include <linux/kernel.h>
+#include <linux/module.h>
 #include <linux/gpio.h>
-int tpd12s015_power_up_sink (bool pwr_state)
+#include <linux/platform_device.h>
+#include <video/omapdss.h>
+
+#define DRV_NAME "tpd12s015"
+
+static struct {
+	int ct_cp_hpd_gpio;
+	int ls_oe_gpio;
+	int hpd_gpio;
+} tpd12s015;
+
+void tpd12s015_power_up_sink(bool pwr_state)
 {
-	int r;
-	r = gpio_set_value(hdmi.ct_cp_hpd_gpio, pwr_state);
-	if (r)
-		DSSERR("Unable to power up sink");
-	return r;
+	gpio_set_value(tpd12s015.ct_cp_hpd_gpio, pwr_state);
 }
 EXPORT_SYMBOL(tpd12s015_power_up_sink);
 
-int tpd12s015_enable_data_link (bool enable)
+void tpd12s015_enable_data_link(bool enable)
 {
-	int r;
-	r = tpd12s015_power_up_sink(enable);
-	if (r) {
-		return r;
-	r = gpio_set_value(ls_oe, enable);
-	if (r)
-		DSSERR("Unable to enable HDMI data link");
-	return r;
-	
+	tpd12s015_power_up_sink(enable);
+	gpio_set_value(tpd12s015.ls_oe_gpio, enable);
 }
 EXPORT_SYMBOL(tpd12s015_enable_data_link);
 
-static int __init tpd12s015_probe (platform device *pdev)
+
+void tpd12s015_detect()
 {
-	int r;
+}
+EXPORT_SYMBOL(tpd12s015_detect);
+
+static int __init tpd12s015_probe(struct platform_device *pdev)
+{
+	int r = 0;
+#if 1
+	printk(KERN_ERR ">>>>>>>>>>>%s", __func__);
+#else
 	struct gpio gpios[] = {
-		{ hdmi.ct_cp_hpd_gpio, GPIOF_OUT_INIT_LOW, "hdmi_ct_cp_hpd" },
-		{ hdmi.ls_oe_gpio, GPIOF_OUT_INIT_LOW, "hdmi_ls_oe" },
-		{ hdmi.hpd_gpio, GPIOF_DIR_IN, "hdmi_hpd" },
+		{ tpd12s015.ct_cp_hpd_gpio, GPIOF_OUT_INIT_LOW, "hdmi_ct_cp_hpd" },
+		{ tpd12s015.ls_oe_gpio, GPIOF_OUT_INIT_LOW, "hdmi_ls_oe" },
+		{ tpd12s015.hpd_gpio, GPIOF_DIR_IN, "hdmi_hpd" },
 	};
+
+	tpd12s015.ct_cp_hpd_gpio = 60;
+	tpd12s015.ls_oe_gpio = 41;
+	tpd12s015.hpd_gpio = 63;
+
 
 	r = gpio_request_array(gpios, ARRAY_SIZE(gpios));
 	if (r) {
-		DSSERR("Request GPIOs failed.");
+		dev_err(&pdev->dev, "Request GPIOs failed");
 		return r;
 	}
 	/* setup interrupt */
-}
-int __init tpd12s015_init_platform_driver(void)
-{
-	return platform_driver_probe(&omapdss_tpd12s015_driver, omapdss_tpd12s015_probe);
+#endif
+	return r;
 }
 
-void __exit tpd12s015_uninit_platform_driver(void)
+static int __exit tpd12s015_remove(struct platform_device *pdev)
 {
-	platform_driver_unregister(&omapdss_hdmihw_driver);
+	printk(KERN_ERR ">>>>>>>>>>>%s", __func__);
+	return 0;
 }
+
+
+static struct platform_driver tpd12s015_driver = {
+	.driver = {
+		.name = DRV_NAME,
+		.owner = THIS_MODULE,
+	},
+	.probe = tpd12s015_probe,
+	.remove = __exit_p(tpd12s015_remove),
+};
+
+module_platform_driver(tpd12s015_driver);
+
+MODULE_AUTHOR("Ricardo Neri <ricardo.neri@ti.com>");
+MODULE_DESCRIPTION("Driver for TPD12S015 HDMI companion chip");
+MODULE_LICENSE("GPL");
+MODULE_ALIAS("platform:" DRV_NAME);
